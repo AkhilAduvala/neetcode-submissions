3c5d7da9cc13479aad58f9ed5edd5ee426158610
AKHIL ADUVALA <aduvalaakhil@gmail.com>
2026-06-01 11:58:10 +0530

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int inputLength = nums.length;
+
+        int[] ans = new int[inputLength * 2];
+
+        // O(2n)
+        for(int i = 0; i < ans.length; i++){
+            ans[i] = nums[i%inputLength];
+        }
+        return ans; //space : O(1), time : O(2n)
+    }
+}
