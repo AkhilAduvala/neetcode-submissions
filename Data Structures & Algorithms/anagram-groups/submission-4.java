de7fc7ca7272aef3d1b0dc769f06f2906148588c
AKHIL ADUVALA <aduvalaakhil@gmail.com>
2026-06-02 22:00:30 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> groups = new HashMap<>();
+
+        // O(n)
+        for(String s : strs){
+            int[] count = new int[26];
+
+            // O(m)
+            for(char c : s.toCharArray()){
+                count[c - 'a']++;
+            }
+
+            String countKey = Arrays.toString(count);
+
+            groups.putIfAbsent(countKey, new ArrayList<>());
+
+            groups.get(countKey).add(s);
+        }
+
+        return new ArrayList<> (groups.values()); // time : O(m * n), space : O(n)
+    }
+}
